02e906be0d83603a32e814a337e243fdb31d3616
Heygeon <heygeonng@gmail.com>
2020-11-17 19:48:10 +0800

    Updated

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import lib as l
+import matplotlib.pyplot as plt
 import streamlit as st
 
 
 st.title("Algo Trading Web App")
 code = st.text_input("Stock Code:")
 stock = l.Stock(code, "max")
-st.write(stock.price)
+
+st.line_chart(stock.price)
