2f3ac16302953adb881e2ea25982480ba5a08f79
Heygeon <heygeonng@gmail.com>
2020-11-18 11:19:38 +0800

    Updated

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ import streamlit as st
 import datetime as dt
 from idea import *
 
-st.title("Algo Trading Web App (Idea Version)")
+
+col1, col2, col3 = st.beta_columns(3)
+
+with col1:
+    st.image("resources\IDEAT Logo 50%.png")
+with col2:
+    st.title("")
+    st.title("Algo Trading Web App")
+
 tmr = dt.date.today()+dt.timedelta(days=1)
 
-start_date = st.date_input("Starting Date")
-end_date = st.date_input("Ending Date (Date of tmr for latest data)", value=tmr)
-code = st.text_input("Stock Code:")
-#sma = st.slider("Select SMA", min_value=5, max_value=100)
+with st.beta_container():
+    start_date = st.date_input("Starting Date")
+    end_date = st.date_input("Ending Date (Date of tmr for latest data)", value=tmr)
+    code = st.text_input("Stock Code:")
+    #sma = st.slider("Select SMA", min_value=5, max_value=100)
 
 stock = l.Stock(code, start=start_date, end=end_date)
 if st.button("Enter"):
