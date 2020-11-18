090800730ccae821fa0dd23160d6cf5c02479c62
Heygeon <heygeonng@gmail.com>
2020-11-18 10:21:19 +0800

    Updated

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import datetime as dt
 from idea import *
 
 st.title("Algo Trading Web App (Idea Version)")
+tmr = dt.date.today()+dt.timedelta(days=1)
 
 start_date = st.date_input("Starting Date")
-end_date = st.date_input("Ending Date")
+end_date = st.date_input("Ending Date (Date of tmr for latest data)", value=tmr)
 code = st.text_input("Stock Code:")
 #sma = st.slider("Select SMA", min_value=5, max_value=100)
 
